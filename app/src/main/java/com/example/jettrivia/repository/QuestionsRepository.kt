99966f7f96c21343c30f248e6d87582be24cfd5e
rahul <ra9853044@gmail.com>
2023-12-19 23:02:59 +0530

    implemented QuestionsRepository

---
+++ app/src/main/java/com/example/jettrivia/repository/QuestionsRepository.kt
@@ -0,0 +1,25 @@
+package com.example.jettrivia.repository
+
+import com.example.jettrivia.data.DataOrException
+import com.example.jettrivia.model.Question
+import com.example.jettrivia.model.QuestionItem
+import com.example.jettrivia.network.QuestionService
+import javax.inject.Inject
+
+class QuestionsRepository @Inject constructor(private val service: QuestionService) {
+
+    private val dataOrException=DataOrException<Question,Boolean,Exception>()
+
+    suspend fun getAllQuestions():DataOrException<Question,Boolean,Exception>{
+        try{
+            dataOrException.loading=true
+            dataOrException.data=service.questionAnswers()
+            if(dataOrException.data.toString().isNotEmpty()) dataOrException.loading=false
+        }
+        catch (e:Exception) {
+            dataOrException.exception=e
+        }
+        return dataOrException
+    }
+
+}
